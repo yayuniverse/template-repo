d5277d54259794e9c9b053f62861651de2ebd9fb
yay <yaytrxsh@proton.me>
2025-05-29 00:00:41 +0100

    chore: update package.json to add linting scripts and update dependencies
    - Added lint and lint:fix scripts for ESLint
    - Updated ESLint and added eslint-config-airbnb-base and eslint-plugin-import
    - Replaced @eslint/js with @eslint/eslintrc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,19 @@
-import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  ...compat.extends("airbnb-base"),
   eslintConfigPrettier,
 ]);
